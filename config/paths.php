49a4ad5dd5d17b2eba12cd7e41f727b40f09a815
Юлия <34940395+drctrue@users.noreply.github.com>
2018-04-11 02:36:53 +0300

    new project

---
+++ config/paths.php
@@ -0,0 +1,8 @@
+<?php
+define('HOST', 'localhost');
+define('USER', 'drctrue');
+define('PASSWORD', 'Kfrektymrf26613');
+define('DATABASE', 'drctrue');
+
+define('HTTP_SERVER', 'http://adyax-test.zzz.com.ua/');
+define('FILE_FOLDER', 'public/uploads/');
