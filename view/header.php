9c52be55d9c3db55a1142174e1573dd56f6dbc40
Юлия <34940395+drctrue@users.noreply.github.com>
2018-04-11 02:49:02 +0300

    new project

--- view/header.php
+++ view/header.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <title>Test</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo HTTP_SERVER; ?>/public/css/style.css">
-    <script src="<?php echo HTTP_SERVER; ?>/public/js/jquery-3.3.1.min.js"></script>
-    <script src="<?php echo HTTP_SERVER; ?>/public/js/custom.js"></script>
+    <link rel="stylesheet" href="<?php echo HTTP_SERVER; ?>public/css/style.css">
+    <script src="<?php echo HTTP_SERVER; ?>public/js/jquery-3.3.1.min.js"></script>
+    <script src="<?php echo HTTP_SERVER; ?>public/js/custom.js"></script>
 	<script src="https://use.fontawesome.com/releases/v5.0.9/js/all.js"></script>
     
 </head>
